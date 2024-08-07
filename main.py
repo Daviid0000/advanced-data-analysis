d3f4ad87c6ecf10e97927736f3d5e045d1c1b221
David Liva <livadavid78@gmail.com>
2024-08-07 11:14:58 -0300

    Se ha encontrado la manera correcta de hallar la mediana y se han corregido los nombres de las variables

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
+# Importaciones
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Obtención de archivo csv
 data = pd.read_csv('MOCK_DATA.csv', delimiter=",")
-
-data["media"] = data["performance_score"].mean()
+salary = data["salary"]
+sumSalary = sum(salary)
+lenSalary = len(salary)
 
 performance_score = data["performance_score"]
-suma = sum(performance_score)
-mediana = sum(performance_score)/len(performance_score)
+sumPerformanceScore = sum(performance_score)
+lenPerformanceScore = len(performance_score)
+
+media = sumPerformanceScore/lenPerformanceScore
+media2 = sumSalary/lenSalary 
+
+
 desvio_estandar = data["performance_score"].std()
 departamentos = data["departament"]
 empleados_por_departamento = data["departament"].value_counts()
 correlacion = data["years_with_company"].corr(data["performance_score"])
+
+desvio_estandar2 = data["salary"].std()
+correlacion2 = data["salary"].corr(data["performance_score"])
+
+sumSalary = data["salary"].sum()
+lenSalary = len(salary)
+ordenSalary = sorted(salary)
+
+# print("Salarios:", salary)
+# print("Salarios ordenados:", ordenSalary)
+# print("Longitud de lista de salarios:", lenSalary)
+# print("Suma de salarios:", sumSalary)
+
+print("La media de Performance Score es:", media)
+if lenPerformanceScore % 2 == 0:
+    performanceScoreMitad = performance_score[lenPerformanceScore//2]
+    performanceScoreMitad2 = performance_score[lenPerformanceScore//2 - 1]
+    mediana2 = (performanceScoreMitad + performanceScoreMitad2)/2
+else:
+    mediana2 = performance_score[lenPerformanceScore//2]
+print("La mediana de puntuación de rendimiento es:", mediana2)
+# print("La mediana de Performance Score es:", mediana)
+print("El desvio estandar de Performance Score es:", desvio_estandar)
+print("La cantidad de empleados de cada departamento es:", empleados_por_departamento)
+print("La correlación entre Years With Company y Performance Score es:", correlacion)
+
+print("La media de Salary es:", media2)
+if lenSalary % 2 == 0:
+    salaryMitad = salary[lenSalary//2]
+    salaryMitad2 = salary[lenSalary//2 - 1]
+    mediana2 = (salaryMitad + salaryMitad2)/2
+else:
+    mediana2 = salary[lenSalary//2]
+print("la mediana de los salarios es:", mediana2)
+print("El desvio estandar de Salary es:", desvio_estandar2)
+print("La correlación entre Salary y Performance Score es:", correlacion2)
 
 # Histograma del performance_score para cada departamento
 departamentos = data["departament"].unique()
@@ -39,8 +83,3 @@ plt.ylabel('Performance Score')
 plt.title('Salary vs. Performance Score')
 plt.show()
 
-
-print("mediana:", mediana)
-print("desvio estandar:", desvio_estandar)
-print("empleados:", empleados_por_departamento)
-print("correlación1 :", correlacion)
